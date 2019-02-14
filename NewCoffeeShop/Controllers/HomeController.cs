de08072da852dda05bf6f278c125816874662f5c
EvanSimon <evansimon07@gmail.com>
2019-02-14 09:46:39 -0500

    Coffee

---
+++ NewCoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewCoffeeShop.Models;
+
+namespace NewCoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+        public ActionResult RegisterUsers()
+        {
+            return View();
+        }
+
+        public ActionResult Confirn()
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ NewCoffeeShop/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewCoffeeShop.Models
+{
+    public class User
+    {
+        [Required] // 
+        [RegularExpression(@"^[a-zA-Z]{2,}$")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{2,}$")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-z0-9]{5,30}@[a-z]{5,10}.[a-z]{2,3}$")]
+        public string Email { set; get; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9]{5,}$")]
+        public string Password { set; get; }
+
+        [Required]
+        [RegularExpression(@"^\d{10}")]
+        public string PhoneNumber { set; get; }
+
+        [Required]
+        [RegularExpression(@"^(((0?[1-9]|1[012])/(0?[1-9]|1\d|2[0-8])|(0?[13456789]|1[012])/(29|30)|(0?[13578]|1[02])/31)/(19|[2-9]\d)\d{2}|0?2/29/((19|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([2468][048]|[3579][26])00)))$")]
+        public string Birthday { set; get; }
+    }
+}
